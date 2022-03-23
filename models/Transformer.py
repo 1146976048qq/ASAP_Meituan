ae1e2e3d4a35741d8fef788bda658c1ed8185110
kzhang <1146976048@qq.com>
2022-03-23 22:46:53 +0800

    Update Transformer.py

--- models/Transformer.py
+++ models/Transformer.py
@@ -106,15 +106,6 @@ class Scaled_Dot_Product_Attention(nn.Module):
         super(Scaled_Dot_Product_Attention, self).__init__()
 
     def forward(self, Q, K, V, scale=None):
-#         '''
-#         Args:
-#             Q: [batch_size, len_Q, dim_Q]
-#             K: [batch_size, len_K, dim_K]
-#             V: [batch_size, len_V, dim_V]
-#             scale: 缩放因子 论文为根号dim_K
-#         Return:
-#             self-attention后的张量，以及attention张量
-#         '''
         attention = torch.matmul(Q, K.permute(0, 2, 1))
         if scale:
             attention = attention * scale
